30c37ffea4d6318a968275785dee70c48f4555e3
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-04-20 17:10:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   end
 
   def add_song_by_name(name)
-    song = Song.new(name)
+    song = Song.new(name, self)
     self.songs << song
     song.artist = self
     @@song_count +=1
